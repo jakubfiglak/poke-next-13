3d48598346627162ec9918f57b7137b701c8ffd7
jakubfiglak <jakub.figlak@netguru.pl>
2023-05-16 14:56:39 +0200

    feat: hover effect in pokemon card

--- src/modules/pokemon/components/pokemon-card.tsx
+++ src/modules/pokemon/components/pokemon-card.tsx
@@ -12,23 +12,26 @@ export const PokemonCard = async ({ name, className }: PokemonCardProps) => {
   const data = await pokemon.getByName(name);
 
   return (
-    <Link
-      href={`/pokemon/${name}`}
+    <div
       className={twMerge(
         "p-4 border rounded-md text-center flex flex-col",
         className
       )}
     >
-      <h3 className="uppercase mb-2">{name}</h3>
-      {data.sprites.front_default && (
-        <Image
-          alt={data.name}
-          src={data.sprites.front_default}
-          height={80}
-          width={80}
-          className="mx-auto mb-2"
-        />
-      )}
+      <Link href={`/pokemon/${name}`} className="group">
+        <h3 className="uppercase mb-2 group-hover:text-cyan-300 transition-colors">
+          {name}
+        </h3>
+        {data.sprites.front_default && (
+          <Image
+            alt={data.name}
+            src={data.sprites.front_default}
+            height={80}
+            width={80}
+            className="mx-auto mb-2 group-hover:scale-110 transition"
+          />
+        )}
+      </Link>
       <div className="flex gap-2 flex-wrap mt-auto">
         {data.types.map(({ type: { name } }) => (
           <span key={name} className="border rounded-full px-3 py-1 text-xs">
@@ -36,6 +39,6 @@ export const PokemonCard = async ({ name, className }: PokemonCardProps) => {
           </span>
         ))}
       </div>
-    </Link>
+    </div>
   );
 };
